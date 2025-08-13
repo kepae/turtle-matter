744e69aa03837c17da624e55b55bf06f82ccb216
Kevin <kevin.paeth@gmail.com>
2025-08-13 13:21:43 -0300

    Update example generation, remove analysis

--- generate_examples.sh
+++ generate_examples.sh
@@ -4,7 +4,7 @@
 RDF_FILE="docs/examples/croissant/inputs/croissant.ttl"
 NAMESPACE="http://mlcommons.org/croissant/"
 DOCS_DIR="docs/examples/croissant/inputs/docs"
-EXAMPLES_OUTPUTS_DIR="docs/examples"
+EXAMPLES_OUTPUTS_DIR="docs/examples/croissant/outputs"
 
 mkdir -p $EXAMPLES_OUTPUTS_DIR
 
@@ -13,6 +13,5 @@ echo "Generating examples for GitHub Pages..."
 uv run python main.py $RDF_FILE --emit html --namespace $NAMESPACE --docs-dir $DOCS_DIR --title "Croissant" > $EXAMPLES_OUTPUTS_DIR/croissant.html
 uv run python main.py $RDF_FILE --emit markdown --namespace $NAMESPACE --docs-dir $DOCS_DIR --title "Croissant" > $EXAMPLES_OUTPUTS_DIR/croissant.md
 uv run python main.py $RDF_FILE --emit jsonld --namespace $NAMESPACE > $EXAMPLES_OUTPUTS_DIR/croissant-context.json
-uv run python main.py $RDF_FILE --namespace $NAMESPACE > $EXAMPLES_OUTPUTS_DIR/croissant-analysis.txt
 
 echo "Done! Examples generated in $EXAMPLES_OUTPUTS_DIR"
